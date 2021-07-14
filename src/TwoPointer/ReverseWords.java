c87df2d86859302a7f9fe480eb451c259dfabd1c
arokiabhavy <arokia.bhavya@gmail.com>
2021-07-14 08:43:23 +0530

    reverseWords

--- src/TwoPointer/ReverseWords.java
+++ src/TwoPointer/ReverseWords.java
@@ -1,6 +1,60 @@
 package TwoPointer;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ReverseWords {
+	@Test
+	public void example1()
+	{
+		Assert.assertEquals("blue is sky the",getReverseWord("the sky is blue"));
+	}
+
+	@Test
+	public void example2()
+	{
+		Assert.assertEquals("world hello",getReverseWord("  hello world  "));
+	}
 	
+	@Test
+	public void example3()
+	{
+		Assert.assertEquals("example good a",getReverseWord("a good   example"));
+	}
+	
+	@Test
+	public void example4()
+	{
+		Assert.assertEquals("Alice Loves Bob",getReverseWord("  Bob    Loves  Alice   "));
+	}
+	
+	@Test
+	public void example5()
+	{
+		Assert.assertEquals("bob like even not does Alice",getReverseWord("Alice does not even like bob"));
+	}
+	
+	private String getReverseWord(String s) {
+		StringBuilder output=new StringBuilder();
+        String[] values=s.split(" ");
+        for(int left=0,right=values.length -1;left<right;)
+        {
+            
+                String temp=values[left];
+                values[left]=values[right];
+                values[right]=temp;
+                left++;
+                right--;
+            
+        }
+        
+        for(String value:values)
+        {
+           if(!value.isEmpty())
+               output.append(value+" ");
+        }
+        
+        return output.toString().trim();
+	}
 
 }
