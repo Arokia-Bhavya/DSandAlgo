76bd11c0236804b2f3b420777087bf10c8514e46
Arokia-Bhavya <arokia.bhavya@gmail.com>
2021-07-31 12:52:39 +0530

    sliding window

---
+++ src/TwoPointer/RemoveduplicatesAtleastTwice.java
@@ -0,0 +1,30 @@
+package TwoPointer;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveduplicatesAtleastTwice {
+	
+	@Test
+	public void example1()
+	{
+		int[] input= {5,5,5,6,6,6,8};
+		int[] output=Arrays.copyOf(input,removeDuplicates(input));
+				
+		Assert.assertArrayEquals();		
+	}
+	
+	private int removeDuplicates(int[] input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Test
+	public void example2()
+	{
+		
+	}
+
+}
