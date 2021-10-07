0553eaa0bd32fadd90f13e0a42e2edfc5aa2ff16
Arokia-Bhavya <arokia.bhavya@gmail.com>
2021-10-07 08:35:11 +0530

    DS Algo

--- src/LinkedList/RemoveLinkedListElement.java
+++ src/LinkedList/RemoveLinkedListElement.java
@@ -1,5 +1,59 @@
 package LinkedList;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RemoveLinkedListElement {
+	
+	@Test
+	public void example1()
+	{
+		ListNode input=new ListNode(1,new ListNode(2,new ListNode(6,new ListNode(3,new ListNode(4,new ListNode(5,new ListNode(6,null)))))));
+		ListNode output=new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5,null)))));
+		Assert.assertTrue(isLinkedListSame(output,RemoveLinkedListElement(input,6)));
+	}
+
+	
+	@Test
+	public void example2()
+	{
+		ListNode input=null;
+		ListNode output=null;
+		Assert.assertTrue(isLinkedListSame(output,RemoveLinkedListElement(input,1)));
+	}
+	
+	@Test
+	public void example3()
+	{
+		ListNode input=new ListNode(7,new ListNode(7,new ListNode(7,new ListNode(7,null))));
+		ListNode output=null;
+		Assert.assertTrue(isLinkedListSame(output,RemoveLinkedListElement(input,7)));
+	}
+	
+	private boolean isLinkedListSame(ListNode output, ListNode input) {
+		while(input!=null && output!=null && input.val==output.val)
+		{
+			input = input.next;
+			output = output.next;
+		}
+		return input==null && output==null;
+	}
+
+	private ListNode RemoveLinkedListElement(ListNode input,int val) {
+		if(input == null)
+			return input;
+		ListNode cur=new ListNode(-1);
+	    cur.next = input;
+	    input = cur;
+		while(cur!=null && cur.next!=null)
+		{
+			if(cur.next.val == val)
+			{
+				cur.next=cur.next.next;
+			}else
+				cur = cur.next;
+		}
+		return input.next;
+	}
 
 }
--- src/dynamicprogramming/PascalsTriangle.java
+++ src/dynamicprogramming/PascalsTriangle.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package dynamicprogramming;
 
 import static org.junit.Assert.assertEquals;
 
