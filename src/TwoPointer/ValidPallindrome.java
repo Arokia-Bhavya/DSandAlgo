69bcaac67772be81f4dd5db770500fdf2d77bb1f
arokiabhavy <arokia.bhavya@gmail.com>
2023-01-07 16:56:37 +0530

    2 Pointer

--- src/TwoPointer/ValidPallindrome.java
+++ src/TwoPointer/ValidPallindrome.java
@@ -19,7 +19,22 @@ public void example2()
 	Assert.assertEquals(false, isValid( "race a car"));	
 }
 
-private boolean isValid(String input) {
+private Object isValid(String input) {
+	StringBuilder cleanInput=new StringBuilder(input.length());
+	for(char value:input.toLowerCase().toCharArray())
+	{
+		if(Character.isLetterOrDigit(value))
+			cleanInput.append(value);
+	}
+	String firstHalf=cleanInput.substring(0, input.length()/2);
+	String secondHalf=cleanInput.reverse().substring(0, input.length()/2);
+	return firstHalf.equals(secondHalf);
+}
+
+
+
+/*TC O(n/2)*/
+private boolean isValidUsing2Pointer(String input) {
 	char[] inputArray=input.toLowerCase().toCharArray();
 	for(int left=0,right=input.length() - 1;left<right;)
 	{
